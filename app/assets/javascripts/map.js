a4cb403bad9e89f50904fb9d16606e270db1a3da
Toby <toby.schaeffer@gmail.com>
2015-05-15 12:23:16 -0400

    merged geojson layer

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,24 +1,57 @@
 function initializeMap(){
   console.log("initializeMap")
-  var map = new L.map('map').setView([-18.906, 47.516], 6);
 
-  L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-      attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-  }).addTo(map);
+  
+  var madaTiles = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+  	attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+    maxZooom: 18  
+  });
 
-  var countryStyle = {
+  $.getJSON($('link[rel="points"]').attr("href"), function(data) {
+    var geojson = L.geoJson(data, {
+      onEachFeature: function (feature, layer) {
+        layer.bindPopup(feature.properties.NAME_2);
+      }
+    });
+
+	var countryStyle = {
     'color': "#000",
     'weight': 2,
     'opacity': 0.6
   };
 
-  function popup(feature, layer) {
-    if (feature.properties && feature.properties.name) {
-        layer.bindPopup(feature.properties.name);
-    }
-  }
+  var map = L.map('map').fitBounds(geojson.getBounds()).setView([-18.906, 47.516], 6); 
+  	madaTiles.addTo(map);
+  	geojson.addTo(map);
 
-
-
+  });
 }
+
+
+
+
+
+
+
+//   var map = new L.map('map').setView([-18.906, 47.516], 6);
+
+//   L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+//       attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+//   }).addTo(map);
+
+//   var countryStyle = {
+//     'color': "#000",
+//     'weight': 2,
+//     'opacity': 0.6
+//   };
+
+//   function popup(feature, layer) {
+//     if (feature.properties && feature.properties.name) {
+//         layer.bindPopup(feature.properties.name);
+//     }
+//   }
+
+
+
+// }
 
